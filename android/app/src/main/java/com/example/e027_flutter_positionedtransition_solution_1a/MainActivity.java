7e8e92b91ee5bbc6b3a298b0ab05f63a66f750b1
moh <osman.elrashid@gmail.com>
2019-08-04 06:21:54 +0400

    initial files

---
+++ android/app/src/main/java/com/example/e027_flutter_positionedtransition_solution_1a/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.e027_flutter_positionedtransition_solution_1a;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
